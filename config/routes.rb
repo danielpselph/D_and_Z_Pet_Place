3332147a7f667f8d45c40591c96df0b6212719f2
Ezekiel Clark <clarkzeke@gmail.com>
2020-02-04 19:25:29 -0700

    Pass test for create new pet

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get '/shelters/:id', to: 'shelters#show'
   post '/shelters', to: 'shelters#create'
   get '/shelters/:id/pets', to: 'shelters#pets'
-  get '/shelters/:id/pets/new', to: 'pets#create'
+  get '/shelters/:id/pets/new', to: 'pets#new'
   get '/shelters/:id/edit', to: 'shelters#edit'
   patch '/shelters/:id', to: 'shelters#update'
   delete '/shelters/:id', to: 'shelters#destroy'
