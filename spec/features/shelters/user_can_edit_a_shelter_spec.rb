6824c0303158c513fd4e22c7788233ab5af1d38e
Daniel Selph <daniel.p.selph@gmail.com>
2020-02-05 15:38:03 -0700

    Create edit shelter from index test and view

---
+++ spec/features/shelters/user_can_edit_a_shelter_spec.rb
@@ -0,0 +1,68 @@
+# User Story 13, Shelter Update From Shelter Index Page
+
+# As a visitor
+# When I visit the shelter index page
+# Next to every shelter, I see a link to edit that shelter's info
+# When I click the link
+# I should be taken to that shelters edit page where I can update its information just like in User Story 5
+
+require 'rails_helper'
+
+describe 'As a visitor' do 
+    describe 'When I visit shelter show page' do
+        before :each do
+            @shelter1 = Shelter.create!(name: 'Sebastians',
+                                        address: '123 Abc St',
+                                        city: 'Denver', 
+                                        state: 'CO', 
+                                        zip: '90210')
+            @shelter2 = Shelter.create!(name: 'bob', 
+                                        address: '1111 Ash St', 
+                                        city: 'Denver', 
+                                        state: 'CO', 
+                                        zip: '80220')
+
+        end
+
+    it 'I should see a link to "Edit"' do 
+
+        visit "/shelters/#{@shelter2.id}"
+
+            expect(current_path).to eq("/shelters/#{@shelter2.id}")
+
+            expect(page).to have_button("Edit")
+
+            click_on "Edit"
+            
+            expect(current_path).to eq("/shelters/#{@shelter2.id}/edit")
+
+            expect(page).to have_content("Name")
+            expect(page).to have_content("Address")
+            expect(page).to have_content("City")
+            expect(page).to have_content("State")
+            expect(page).to have_content("Zip")
+
+            fill_in "Name", with: "Sarahs Pet Sanctuary"
+            fill_in "Address", with: "678 Cactus Dr"
+            fill_in "City", with: "Phoenix"
+            fill_in "State", with: "AZ"
+            fill_in "Zip", with: "85371"
+
+            click_on "Update Shelter"
+
+            expect(current_path).to eq("/shelters/#{@shelter2.id}")
+
+            expect(page).to have_content("Sarahs Pet Sanctuary")
+            expect(page).to have_content("678 Cactus Dr")
+            expect(page).to have_content("Phoenix")
+            expect(page).to have_content("AZ")
+            expect(page).to have_content("85371")
+
+            expect(page).not_to have_content("bob")
+            expect(page).not_to have_content("1111 Ash St")
+            expect(page).not_to have_content("Denver")
+            expect(page).not_to have_content("CO")
+            expect(page).not_to have_content("80220")
+        end
+    end
+end
--- spec/features/shelters/user_can_see_all_shelters_spec.rb
+++ spec/features/shelters/user_can_see_all_shelters_spec.rb
@@ -11,4 +11,29 @@ RSpec.describe "shelters index page", type: :feature do
       expect(page).to have_button("New Shelter")
     end
   end
+
+# User Story 13, Shelter Update From Shelter Index Page
+
+# As a visitor
+# When I visit the shelter index page
+# Next to every shelter, I see a link to edit that shelter's info
+# When I click the link
+# I should be taken to that shelters edit page where I can update its information just like in User Story 5
+
+
+  it 'When I visit the shelter index page, next to every shelter I see a link to edit that shelters info' do
+
+    shelter_1 = Shelter.create(name: "Shelter 1", address: "Place", city: "Metropolis", state: "Denial", zip: "90000")
+
+    visit '/shelters'
+
+    within "#shelter-#{shelter_1.id}" do
+      expect(page).to have_button("Edit")
+      click_on "Edit"
+    end
+
+    expect(current_path).to eq("/shelters/#{shelter_1.id}/edit")
+
+  end 
 end
+
