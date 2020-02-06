441007983520948ee94a75cf26b5ee00ec8de927
Daniel Selph <daniel.p.selph@gmail.com>
2020-02-06 08:02:31 -0700

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  #root
+  get '/', to: 'shelters#index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/shelters', to: 'shelters#index'
   get '/shelters/new', to: 'shelters#new'
