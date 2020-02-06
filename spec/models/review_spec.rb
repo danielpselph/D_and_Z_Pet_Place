1687c3c94e8b0c3a9f415b52db189c78017130be
Daniel Selph <daniel.p.selph@gmail.com>
2020-02-06 12:14:46 -0700

    Add review class

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -5,10 +5,9 @@ describe Review, type: :model do
     it { should validate_presence_of :title }
     it { should validate_presence_of :rating}
     it { should validate_presence_of :content}
-
   end
+  
   describe 'relationships' do
     it { should belong_to :Shelter }
   end
-
 end
