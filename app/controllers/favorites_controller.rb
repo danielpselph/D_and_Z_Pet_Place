876140c368271ced964e55c9f74c32130336c822
Ezekiel Clark <clarkzeke@gmail.com>
2020-02-10 14:03:27 -0700

    Add query params for removing a pet from favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,11 +2,20 @@ class FavoritesController < ApplicationController
   include ActionView::Helpers::TextHelper
 
   def update
-    pet = Pet.find(params[:pet_id])
-    favorites.add_pet(pet.id)
-    session[:favorites] = favorites.contents
-    flash[:notice] = "#{pet.name} is now in your Favorites."
-    redirect_to "/pets/#{pet.id}"
+    # require "pry"; binding.pry
+    if params[:remove]
+      pet = Pet.find(params[:pet_id])
+      session[:favorites] = favorites.contents
+      favorites.contents.delete(pet.id)
+      flash[:notice] = "#{pet.name} has been removed from your Favorites."
+      redirect_back(fallback_location: "/pets")
+    else
+      pet = Pet.find(params[:pet_id])
+      favorites.add_pet(pet.id)
+      session[:favorites] = favorites.contents
+      flash[:notice] = "#{pet.name} is now in your Favorites."
+      redirect_to "/pets/#{pet.id}"
+    end
   end
 
   def index
@@ -17,22 +26,5 @@ class FavoritesController < ApplicationController
       # require "pry"; binding.pry
       @pets = Pet.find(session[:favorites])
     end
-  end
-
-  # def index
-  #   if session[:favorites]
-  #     @pets = Pet.find(session[:favorites])
-  #   else
-  #     flash[:notice] = "No Favorites."
-  #   end
-  # end
-
-  def remove
-    require "pry"; binding.pry
-    pet = Pet.find(params[:pet_id])
-    session[:favorites] = favorites.contents
-    favorites.contents.delete(pet.id)
-    flash[:notice] = "#{pet.name} has been removed from your Favorites."
-    redirect_back(fallback_location: "/pets")
   end
 end
