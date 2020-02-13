b58df8b9b8f651348ac305eb73db68a7db43441a
Ezekiel Clark <clarkzeke@gmail.com>
2020-02-12 18:29:36 -0700

    Add remove delete button from shelter index page

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -12,7 +12,6 @@ class Shelter < ApplicationRecord
   end
 
   def has_pending_pets?
-    require "pry"; binding.pry
     if self.pets.find {|pet| pet.status.include?("Pending")}
       return true
     else
